875da263d10d8c9659b90b1e37816a702f9d32cd
Severyn Lisovskyi <smlisovsky@gmail.com>
2021-01-21 11:52:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ ln -s $(pwd)/.workgitconfig ~/.workgitconfig
 
 ## iterm
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$(pwd)/iterm2"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
