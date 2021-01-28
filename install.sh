bd33bef2da027d91f07c1ee46b2c4947cb65d045
Severyn Lisovskyi <smlisovsky@gmail.com>
2021-01-28 11:11:41 +0100

    mac specific settings

--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
 #!/bin/sh -x
 
-# brew install hstr
-# brew install gpg2 pinentry-mac
-# echo "pinentry-program /opt/homebrew/bin/pinentry-mac" > ~/.gnupg/gpg-agent.conf
-
 ln -s $(pwd)/.zs ~/.zs
 ln -s $(pwd)/.zshrc ~/.zshrc
 ln -s $(pwd)/.gitconfig ~/.gitconfig
 ln -s $(pwd)/.workgitconfig ~/.workgitconfig
 
-## iterm
-# Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$(pwd)/iterm2"
-# Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+if [[ `uname` == "Darwin" ]]; then
+
+  which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew install hstr gpg2 pinentry-mac
+  echo "pinentry-program /opt/homebrew/bin/pinentry-mac" > ~/.gnupg/gpg-agent.conf
+  ## iterm
+  # Specify the preferences directory
+  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$(pwd)/iterm2"
+  # Tell iTerm2 to use the custom preferences in the directory
+  defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+  defaults write -g InitialKeyRepeat -int 25 # normal minimum is 15 (225 ms)
+  defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+fi
